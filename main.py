7b757dc5ca843fb579d6d2d7eb97f624ade2737f
Jzorgui <37659681+Jzorgui@users.noreply.github.com>
2021-11-14 19:25:02 +0100

    main final version

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ phi, e, d = fct.getAliceParameters(p, q)
 
 cryptMessage = fct.encryptMessage(n, e)
 decryptedMessage = fct.decryptMessage(cryptMessage, n, d)
-print("Decrypted message : " + str(decryptedMessage))
+print('Decrypted message : ' + str(decryptedMessage))
 
 
 crackedMessage = fct.crackMessage(cryptMessage, n, e)
-print("Cracked message : " + str(crackedMessage))
+print('Cracked message : ' + str(crackedMessage))
